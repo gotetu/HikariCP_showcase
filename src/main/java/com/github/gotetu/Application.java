3201ad2d0f076ccad44b452a7a46a605f13f9f10
Tomoyuki HIRAI <gotetu@gmail.com>
2020-10-10 23:28:18 +0900

    add Application

--- src/main/java/com/github/gotetu/Application.java
+++ src/main/java/com/github/gotetu/Application.java
@@ -1,11 +1,16 @@
 package com.github.gotetu;
 
-import com.github.gotetu.db.HikariCPDataSource;
-import com.github.gotetu.mybatis.MybatisFactory;
+import com.github.gotetu.domain.model.StravaSummaryActivityModel;
+import com.github.gotetu.domain.service.StravaSummaryActivityService;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        MybatisFactory factory = new MybatisFactory(HikariCPDataSource.getDataSource());
-        // SqlSessionFactory sqlSessionFactory = factory.buildSqlSessionFactory(MapperClass.class);
+        StravaSummaryActivityService service = new StravaSummaryActivityService();
+        List<StravaSummaryActivityModel> result =  service.findAll();
+        for (StravaSummaryActivityModel model : result) {
+            System.out.println(model.toString());
+        }
     }
 }
